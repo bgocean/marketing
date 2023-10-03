1ca671c3053710dc43d7c693923a344333bc0fd8
bgocean <bigocean700@gmail.com>
2023-10-03 21:22:14 +0300

    final commit

--- js/main.js
+++ js/main.js
@@ -1,4 +1,10 @@
 $(function () {
-  let mixer = mixitup('.portfolio__content');
-  });
+    let mixer = mixitup(".portfolio__content");
+
+    $(".slider-blog__inner").slick({
+        dots: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="./img/arrow-left.svg" alt="arrow-left"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="./img/arrow-right.svg" alt="arrow-right"></button>'
+    });
+});
 
