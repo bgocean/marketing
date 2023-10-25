f21fea524d6d70e999a3adb02bfb3173368fc121
bgocean <bigocean700@gmail.com>
2023-10-25 22:48:19 +0300

    media #2

--- js/main.js
+++ js/main.js
@@ -1,10 +1,15 @@
 $(function () {
-    let mixer = mixitup(".portfolio__content");
+  $(".slider-blog__inner").slick({
+    dots: true,
+    prevArrow:
+      '<button type="button" class="slick-prev"><img src="./img/arrow-left.svg" alt="arrow-left"></button>',
+    nextArrow:
+      '<button type="button" class="slick-next"><img src="./img/arrow-right.svg" alt="arrow-right"></button>',
+  });
 
-    $(".slider-blog__inner").slick({
-        dots: true,
-        prevArrow: '<button type="button" class="slick-prev"><img src="./img/arrow-left.svg" alt="arrow-left"></button>',
-        nextArrow: '<button type="button" class="slick-next"><img src="./img/arrow-right.svg" alt="arrow-right"></button>'
-    });
+  $(".menu__btn").on("click", function () {
+    $(".header__top-inner").toggleClass("header__top-inner--active");
+  });
+
+  let mixer = mixitup(".portfolio__content");
 });
-
