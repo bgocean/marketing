8151c8d5a99b80f000291bad2619ab88a9cc15d1
bgocean <bigocean700@gmail.com>
2023-11-05 00:47:54 +0200

    final+

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 $(function () {
+  $(".menu a, .go-top").on("click", function (e) {
+    //отменяем стандартную обработку нажатия по ссылке
+    e.preventDefault();
+
+    //забираем идентификатор блока с атрибута href
+    let id = $(this).attr("href"),
+      //узнаем высоту от начала страницы до блока на который ссылается якорь
+      top = $(id).offset().top;
+
+    //анимируем переход на расстояние - top за 1500 мс
+    $("body,html").animate({ scrollTop: top }, 1500);
+  });
+
   $(".slider-blog__inner").slick({
     dots: true,
     prevArrow:
@@ -15,7 +28,7 @@ $(function () {
     ],
   });
 
-  $(".menu__btn").on("click", function () {
+  $(".menu__btn, .menu a").on("click", function () {
     $(".header__top-inner").toggleClass("header__top-inner--active");
   });
 
